94b812f8969aea7326e908e7af4595afb51ca2a4
Just another programmer <alecgmcnamara@gmail.com>
2026-01-29 13:21:01 +0000

    Add files via upload
    Minimused a bit more

--- assets/app.js
+++ assets/app.js
@@ -1,26 +1,12 @@
 // *** Use Ctrl + Shift + i to start debug tools in Chrome ***
 
-var connected = false;
 var counter1 = 0;
 var counter2 = 0;
 
-const socket = io(`http://${window.location.host}`); // create socket.io connection
+const socket = io();
+setInterval(handleSendMessage, 1000);
 
-// Start the application
-document.addEventListener('DOMContentLoaded', () => {
-    initSocketIO();
-    setInterval(handleSendMessage, 1000); //every 1 second
-});
-
-function initSocketIO() {
-    socket.on('connect', () => {
-        //console.log("Connected");
-        connected = true;
-    });
-
-    socket.on('message_from_q', (message) => {
-        //console.log("received message from Q, ");
-        //console.log(message);
+socket.on('message_from_q', (message) => {
         document.getElementById("S1").innerHTML = message["S1"];
         document.getElementById("S2").innerHTML = message["S2"];
         document.getElementById("S3").innerHTML = message["S3"];
@@ -35,15 +21,7 @@ function initSocketIO() {
         counter2 = message["S2"];
      });
 
-    socket.on('disconnect', () => {
-        //console.log("Disconnected");
-        connected = false;
-    });
-}
-
 function handleSendMessage() {
-    if (connected)
-    {
         counter1 +=1;  // increment counter1 on browser side
         var message = {"S1": counter1,
                         "S2": counter2,
@@ -56,7 +34,5 @@ function handleSendMessage() {
                         "S9": "90",
                         "S10": "100"
                       };
-        //console.log("Sending message from browser");
         socket.emit('message_from_browser', message);
         }
-    }
